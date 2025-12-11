ea81e89fb74f9f68dc3910b2f85670076ecb48f8
ojasv31 <ojasv31@student.ubc.ca>
2025-12-10 18:29:13 -0800

    sample website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Update footer year automatically
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
